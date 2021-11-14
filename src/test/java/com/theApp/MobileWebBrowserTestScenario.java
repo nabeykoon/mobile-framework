b40f9e61bda91ff920291da871febb3040264ad8
Nadeera Abeykoon <nabeykoon@gmail.com>
2021-11-14 23:52:36 +0530

    Fixes to web browser test

--- src/test/java/com/theApp/MobileWebBrowserTestScenario.java
+++ src/test/java/com/theApp/MobileWebBrowserTestScenario.java
@@ -2,6 +2,9 @@ package com.theApp;
 
 import com.mobile.core.base.TestUtilities;
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
@@ -14,10 +17,9 @@ public class MobileWebBrowserTestScenario extends TestUtilities {
     @Test
     public void testAppiumWebPage() {
         //simple test written to test web browser testing without page Objects
-        WebDriverWait wait = new WebDriverWait (getRemoteDriver (), 10);
+        WebDriverWait wait = new WebDriverWait (getRemoteDriver (), 30);
         getRemoteDriver ().get (siteUrl);
-        wait.until (ExpectedConditions.presenceOfElementLocated (By.id ("toggleMenu"))).click ();
-        sleep (1000);
+        wait.until (ExpectedConditions.visibilityOfElementLocated (By.id("toggleMenu"))).click();
         wait.until (ExpectedConditions.presenceOfElementLocated (By.linkText ("Contact"))).click ();
         wait.until (ExpectedConditions.presenceOfElementLocated (By.id ("contactEmail"))).sendKeys ("nadeera19848@yahoo.com");
         getRemoteDriver ().findElement (By.id ("contactText")).sendKeys ("test Message");
