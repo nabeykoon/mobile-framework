2673a393867bd263b104b17b85194ef954f9a189
Nadeera Abeykoon <nabeykoon@gmail.com>
2021-11-15 01:35:40 +0530

    Fix web Browser test using javascriptexecutor click

--- src/test/java/com/theApp/MobileWebBrowserTestScenario.java
+++ src/test/java/com/theApp/MobileWebBrowserTestScenario.java
@@ -4,10 +4,8 @@ import com.mobile.core.base.TestUtilities;
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
-import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class MobileWebBrowserTestScenario extends TestUtilities {
@@ -18,9 +16,12 @@ public class MobileWebBrowserTestScenario extends TestUtilities {
     public void testAppiumWebPage() {
         //simple test written to test web browser testing without page Objects
         WebDriverWait wait = new WebDriverWait (getRemoteDriver (), 30);
+        JavascriptExecutor ex=(JavascriptExecutor)getRemoteDriver();
         getRemoteDriver ().get (siteUrl);
-        wait.until (ExpectedConditions.visibilityOfElementLocated (By.id("toggleMenu"))).click();
-        wait.until (ExpectedConditions.presenceOfElementLocated (By.linkText ("Contact"))).click ();
+        WebElement toggleMenu = wait.until (ExpectedConditions.presenceOfElementLocated (By.id("toggleMenu")));
+        ex.executeScript("arguments[0].click()", toggleMenu);
+        WebElement contactMenu = wait.until (ExpectedConditions.presenceOfElementLocated (By.linkText ("Contact")));
+        ex.executeScript("arguments[0].click()", contactMenu);
         wait.until (ExpectedConditions.presenceOfElementLocated (By.id ("contactEmail"))).sendKeys ("nadeera19848@yahoo.com");
         getRemoteDriver ().findElement (By.id ("contactText")).sendKeys ("test Message");
         getRemoteDriver ().findElement (By.xpath ("//input[@value='Send']")).click ();
